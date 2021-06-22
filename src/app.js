37bb21b72cc352e62cdcc9ebcc2d69a5f8d52e39
Dongkyun_Jang <thd6576@naver.com>
2021-06-22 20:29:25 +0900

    Add prototype searchbar

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 import Header from './components/Header';
+import Searchbar from './components/Searchbar';
 
 function App() {
+  const CAT_API = 'https://api.thecatapi.com/v1';
+
+  async function getCatdata(name) {
+    const URL = `${CAT_API}/breeds/search?q=${name}`;
+    const data = await fetch(URL, {
+      method: 'get',
+      headers: {
+        'x-api-key': 'DEMO-API-KEY',
+      },
+    });
+    const response = await data.json();
+    const description = await response[0].description;
+    document.body.append(description);
+  }
+
   document.getElementById('header').innerHTML = Header();
+
+  document.getElementById('searchbar').innerHTML = Searchbar();
+  document.getElementById('searchcat').addEventListener('click', () => {
+    const name = document.querySelector('#searchcat input').value;
+    getCatdata(name);
+  });
 }
 
 App();
