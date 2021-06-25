7a580db491080f1b8c496c99a31c8e869c83603c
Dongkyun_Jang <thd6576@naver.com>
2021-06-25 13:06:06 +0900

    feat: show images on mainpage

---
+++ src/api/theCatAPI.js
@@ -0,0 +1,3 @@
+const API_ENDPOINT = 'https://api.thecatapi.com/v1';
+
+export default API_ENDPOINT;
---
+++ src/components/Searchbar.js
@@ -0,0 +1,10 @@
+export default function Searchbar() {
+  const template = `
+        <form id="searchcat">
+            <input type="text">
+            <button type="button">고양이 찾기</button>
+        </form>
+    `;
+
+  return template;
+}
